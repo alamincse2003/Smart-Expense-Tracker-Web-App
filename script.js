c1eb8db8bca71c620523351a649b77f3690dbb08
alamincse2003 <mdalamincse2003@gmail.com>
2025-07-29 23:40:30 +0600

    total expense and expense design with script file integrate

---
+++ script.js
@@ -0,0 +1,18 @@
+// elements
+const form = document.getElementById("expense-form");
+const inputAmount = document.getElementById("amount");
+const inputNote = document.getElementById("note");
+const selectCategory = document.getElementById("category");
+const inputDate = document.getElementById("inputDate");
+const totalExpense = document.getElementById("totalExpense");
+const expenseList = document.getElementById("expenseList");
+
+console.log(
+  form,
+  inputAmount,
+  inputNote,
+  selectCategory,
+  inputDate,
+  totalExpense,
+  expenseList
+);
